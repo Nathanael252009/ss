17e1ae8009987183f0d41514e712465d287e3368
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-31 13:27:27 +0300

    changeQuantity function works properly

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,9 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./styles.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { changeQuantity } from "../../redux/cardsSlice";
 
 function Cards() {
+  const dispatch = useDispatch();
   const myitems = useSelector((state) => state.cards.items);
+
+  const handleChangeQuantity = (id, targetvalue) => {
+    console.log("cardid", id);
+    console.log("cardvalue", targetvalue);
+    dispatch(changeQuantity({ id, targetvalue }));
+  };
+
   return (
     <div className={styles.gridContainer}>
       {myitems.map((item) => (
@@ -15,7 +24,12 @@ function Cards() {
           </p>
           <div className={styles.buyOptionsDiv}>
             <button className={styles.sellBtn}>Sell</button>
-            <input className={styles.priceInput} type="number" />
+            <input
+              className={styles.priceInput}
+              type="number"
+              value={item.quantity}
+              onChange={(e) => handleChangeQuantity(item.id, e.target.value)}
+            />
             <button className={styles.buyBtn}>Buy</button>
           </div>
         </div>
--- src/redux/cardsSlice.js
+++ src/redux/cardsSlice.js
@@ -12,76 +12,110 @@ export const cardsSlice = createSlice({
         title: "Big Mac",
         price: 2,
         img: "https://neal.fun/spend/images/big-mac.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Year of Netflix",
         price: 100,
         img: "https://neal.fun/spend/images/year-of-netflix.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Smartphone",
         price: 700,
         img: "https://neal.fun/spend/images/smartphone.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Acre of Farmland",
         price: 2500,
         img: "https://neal.fun/spend/images/acre-of-farmland.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Luxury Wine",
         price: 5000,
         img: "https://neal.fun/spend/images/luxury-wine.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Jet Ski",
         price: 10000,
         img: "https://neal.fun/spend/images/jet-ski.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Tesla",
         price: 50000,
         img: "https://neal.fun/spend/images/tesla.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "House",
         price: 250000,
         img: "https://neal.fun/spend/images/single-family-home.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Boeing 747",
         price: 150000000,
         img: "https://neal.fun/spend/images/boeing-747.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Skyscraper",
         price: 750000000,
         img: "https://neal.fun/spend/images/skyscraper.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "Cruise Ship",
         price: 930000000,
         img: "https://neal.fun/spend/images/cruise-ship.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
       {
         id: nanoid(),
         title: "NBA Team",
         price: 2120000000,
         img: "https://neal.fun/spend/images/nba-team.jpg",
+        quantity: 0,
+        anyBuyed: false,
       },
     ],
   },
-  reducers: {},
+  reducers: {
+    changeQuantity: (state, action) => {
+      const id = action.payload.id;
+      const targetvalue = action.payload.targetvalue;
+      const updatedItems = state.items.map((item) =>
+        item.id === id ? { ...item, quantity: targetvalue } : item
+      );
+      state.items = updatedItems;
+    },
+  },
 });
 
+export const { changeQuantity } = cardsSlice.actions;
 export default cardsSlice.reducer;
