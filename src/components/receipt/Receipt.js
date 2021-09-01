3e31d633a329ad971d71aebb13e3e9c0311708c0
hakanolgun <kaptanhakanolgun@gmail.com>
2021-09-01 13:13:21 +0300

    receipt section exist  on screen only if any item buyed

--- src/components/receipt/Receipt.js
+++ src/components/receipt/Receipt.js
@@ -6,27 +6,30 @@ function Receipt() {
   const receiptItems = useSelector((state) => state.cards.receiptItems);
   const total = useSelector((state) => state.cards.totalReceipt);
 
-  return (
-    <div className={styles.receiptContainerDiv}>
-      <h2>Your Receipt</h2>
-      {receiptItems.map((item) => (
-        <p className={styles.receiptItemInfo}>
-          <span className={styles.receiptItemTitle}>{item.title}</span>
-          <span className={styles.receiptItemQuantity}>x{item.quantity}</span>
-          <span className={styles.receiptItemPrice}>
-            $
-            {Number(item.price * item.quantity)
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
-          </span>
+  if (total && total > 0) {
+    return (
+      <div className={styles.receiptContainerDiv}>
+        <h2>Your Receipt</h2>
+        {receiptItems.map((item) => (
+          <p className={styles.receiptItemInfo}>
+            <span className={styles.receiptItemTitle}>{item.title}</span>
+            <span className={styles.receiptItemQuantity}>x{item.quantity}</span>
+            <span className={styles.receiptItemPrice}>
+              $
+              {Number(item.price * item.quantity)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
+            </span>
+          </p>
+        ))}
+        <p className={styles.totalContainer}>
+          <span>Total:</span>
+          <span>${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span>
         </p>
-      ))}
-      <p className={styles.totalContainer}>
-        <span>Total:</span>
-        <span>${total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span>
-      </p>
-    </div>
-  );
+      </div>
+    );
+  }
+  return <div></div>;
 }
 
 export default Receipt;
